dbff109e8d08734000cb800c5f5cf4b3e67c4641
Shankar Rao Pandala <shankarpandala@users.noreply.github.com>
2021-02-17 00:13:02 +0530

    fix error in test cases

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,13 +3,3 @@ from lazyprofiler import cli
 def test_cli_template():
     pass
     # assert cli.cli() is None
-
-from lazyprofiler import log_gpu_cpu_stats
-
-def test_log_gpu_cpu_stats():
-    pass
-
-from lazyprofiler import GetStats
-
-def test_GetStats():
-    pass
